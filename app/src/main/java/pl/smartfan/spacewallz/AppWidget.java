4016a5a51c3fda937f6ee4049e3c0c5eb68e0436
Piotr Pesta <piotr.pesta@gmail.com>
2018-01-02 15:59:36 +0100

    Code cleanup.

--- app/src/main/java/pl/smartfan/spacewallz/AppWidget.java
+++ app/src/main/java/pl/smartfan/spacewallz/AppWidget.java
@@ -12,25 +12,10 @@ import android.widget.RemoteViews;
  */
 public class AppWidget extends AppWidgetProvider {
 
-//    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-//                                int appWidgetId) {
-//
-//        CharSequence widgetText = context.getString(R.string.appwidget_text);
-//        // Construct the RemoteViews object
-//        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.app_widget);
-//        views.setTextViewText(R.id.appWidgetText, widgetText);
-//
-//        // Instruct the widget manager to update the widget
-//        appWidgetManager.updateAppWidget(appWidgetId, views);
-//    }
-
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
-            //updateAppWidget(context, appWidgetManager, appWidgetId);
-
-
             //Start MainActivity after click on widget
             RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.app_widget);
             Intent configIntent = new Intent(context, MainActivity.class);
